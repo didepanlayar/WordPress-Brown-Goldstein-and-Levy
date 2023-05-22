b75a61a89967527cb10ed868c511cc4a16e2b49b
Albin Maulana <albinmaulana@yahoo.com>
2023-05-22 16:25:45 +0700

    Feature: Add Google Fonts Configuration

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -1,5 +1,6 @@
 <?php
 
 function get_enqueue() {
-    wp_enqueue_style('stylesheet', get_stylesheet_uri());
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600&family=Open+Sans&display=swap', [], false);
+    wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], false);
 }
