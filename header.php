d74c6a8e22515d98ecf6351d97476eafe905a137
Albin Maulana <albinmaulana@yahoo.com>
2023-05-23 17:13:10 +0700

    Refactor: Add Logo Wrapper

--- header.php
+++ header.php
@@ -9,3 +9,15 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+    <header class="site-header">
+        <div class="container">
+            <div class="d-flex">
+                <div class="logo-wrapper">
+                    <?php $header_logo = get_field('header_logo', 'option'); ?>
+                    <a href="<?php esc_url(home_url()); ?>" class="logo-link">
+                        <img src="<?php echo $header_logo['url']; ?>" alt="<?php echo $header_logo['title']; ?>">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
