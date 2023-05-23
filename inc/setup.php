90de8b7f16bff164d22a33f295a7af30ce93d1d0
Albin Maulana <albinmaulana@yahoo.com>
2023-05-23 18:00:35 +0700

    Refactor: Register Nav Menus

--- inc/setup.php
+++ inc/setup.php
@@ -4,4 +4,13 @@ function start_setup() {
     add_theme_support('html5');
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+    add_theme_support('menus');
+    // Menus
+    register_nav_menus([
+        'header-menu'           => __('Header Menu', 'browngoldsteinlevy'),
+        'footer-col-one-menu'   => __('Footer Column 1', 'browngoldsteinlevy'),
+        'footer-col-two-menu'   => __('Footer Column 2', 'browngoldsteinlevy'),
+        'footer-col-three-menu' => __('Footer Column 3', 'browngoldsteinlevy'),
+        'privacy-menu'          => __('Privacy Menu', 'browngoldsteinlevy')
+    ]);
 }
