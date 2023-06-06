e2e4ab2e17f7ea2981664c2d68e740d084037e13
Albin Maulana <albinmaulana@yahoo.com>
2023-06-06 17:58:43 +0700

    Feature: Add History Section on What We Stand For Page

--- about.php
+++ about.php
@@ -22,8 +22,37 @@
     <section class="mission" style="background-image: url(<?php echo $our_mission_background['url']; ?>">
         <div class="container">
             <div class="mission__content text-center">
-                <h2 class="mission-title mission-title-sep"><?php echo $our_mission_title; ?><span></span></h2>
+                <h2 class="section-title section-title-sep"><?php echo $our_mission_title; ?><span></span></h2>
                 <p class="mission-description"><?php echo $our_mission_description; ?></p>
+            </div>
+        </div>
+    </section>
+    <!-- History -->
+    <?php
+        $our_history_title       = get_field('our_history_title');
+        $our_history_description = get_field('our_history_description');
+        $our_history_blockquote  = get_field('our_history_blockquote');
+        $our_history_button      = get_field('our_history_button');
+    ?>
+    <section class="history">
+        <div class="container">
+            <div class="history__content">
+                <h2 class="section-title section-title-sep"><?php echo $our_history_title; ?><span></span></h2>
+                <div class="history__content-wrapper d-flex space-between">
+                    <div class="history__content-description">
+                        <p><?php echo $our_history_description; ?></p>
+                    </div>
+                    <div class="history__content-blockquote">
+                        <blockquote><?php echo $our_history_blockquote; ?></blockquote>
+                    </div>
+                </div>
+                <?php
+                    if($our_history_button) {
+                ?>
+                        <a href="<?php echo $our_history_button['url']; ?>" class="btn btn-outline-grey btn-has-icon d-i-flex item-center justify-center" target="<?php echo $our_history_button['target']; ?>"><?php echo $our_history_button['title']; ?> <img src="<?php echo get_template_directory_uri() . '/assets/images/Right-Arrow-Primary.svg' ?>" alt="Right Arrow Primary"></a>
+                <?php
+                    }
+                ?>
             </div>
         </div>
     </section>
