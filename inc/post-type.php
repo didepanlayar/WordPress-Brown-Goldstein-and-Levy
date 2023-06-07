558598bbd0c1d0c43948aa80b59488110711ff87
Albin Maulana <albinmaulana@yahoo.com>
2023-06-07 19:51:57 +0700

    Feature: Add Team Post Type

--- inc/post-type.php
+++ inc/post-type.php
@@ -31,4 +31,33 @@ function custom_post_type() {
         'menu_icon'          => 'dashicons-media-document',
 		'supports'           => ['title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments']
     ]);
+    // Team
+	register_post_type('team', [
+        'labels'             => [
+            'name'                  => _x('Team', 'browngoldsteinlevy'),
+            'singular_name'         => _x('Team', 'browngoldsteinlevy'),
+            'menu_name'             => _x('Team', 'browngoldsteinlevy'),
+            'name_admin_bar'        => _x('Team', 'browngoldsteinlevy'),
+            'add_new'               => __('Add New', 'browngoldsteinlevy'),
+            'add_new_item'          => __('Add New', 'browngoldsteinlevy'),
+            'new_item'              => __('New Member', 'browngoldsteinlevy'),
+            'edit_item'             => __('Edit Member', 'browngoldsteinlevy'),
+            'view_item'             => __('View Member', 'browngoldsteinlevy'),
+            'view_items'            => __('View Team', 'browngoldsteinlevy'),
+            'all_items'             => __('All Members', 'browngoldsteinlevy'),
+        ],
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+        'show_in_rest'       => true,
+		'query_var'          => true,
+		'rewrite'            => ['slug' => 'team'],
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+        'menu_icon'          => 'dashicons-groups',
+		'supports'           => ['title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments']
+    ]);
 }
